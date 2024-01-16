5a0236866d36d059cadf1e3d3b6c25fc0609ea5f
Hai Hu <hu.hai@outlook.com>
2024-01-16 09:08:39 +0800

    Add files via upload

--- remove_bad.py
+++ remove_bad.py
@@ -3,8 +3,11 @@ lines = open('GoldenPairs-InitialVersion.tex').readlines()
 i=0
 old=[]
 while i < len(lines):
-    if lines[i][0].isdigit(): old.append((lines[i][3:].strip(), lines[i+1][3:].strip(), lines[i+1][0]))
-    i+=3
+    if lines[i][0].isdigit(): 
+        old.append((lines[i][3:].strip(), lines[i+1][3:].strip(), lines[i+1][0]))
+        i+=2
+    else:
+        i+=1
 print(len(old))
 
 # get to-remove pairs in tuples (p, h, label)
@@ -12,8 +15,8 @@ lines2 = open('gold_30_ver2_gpt-4_6shots_reasonTrue_wrongs.txt').readlines()
 j=0
 rem = []
 while j < len(lines2):
-    if lines2[j].startswith('Premise: '):
-        pair = (lines2[j][8:].strip(), 
+    if lines2[j].startswith('[remove]Premise: '):
+        pair = (lines2[j][16:].strip(), 
                     lines2[j+1][11:].strip(),
                     lines2[j+2][7].upper())
         assert pair[2] in ['E','C','N']
@@ -35,6 +38,10 @@ print(count)
 # ('We combine two recent ideas: cartesian differential categories, and restriction categories.', 'We combine three ideas', 'N')
 # manually corrected
 
+# print all removed
+print('\n\nREMOVED:')
+for r in rem: print(f'P: {r[0]}\nH: {r[1]}\nLabel: {r[2]}\n')
+
 # remove 
 new = []
 for i in old:
@@ -49,6 +56,36 @@ for i in old:
 
 print(len(old), len(rem), len(new))
 
-with open('gold-final.txt','w') as f:
-    for i, pair in enumerate(new):
-        f.write(f'ID: {i}\nPremise: {pair[0]}\nHypothesis: {pair[1]}\nLabel: {pair[2]}\n\n')
+# with open('gold-final.txt','w') as f:
+#     for i, pair in enumerate(new):
+        # f.write(f'ID: {i}\nPremise: {pair[0]}\nHypothesis: {pair[1]}\nLabel: {pair[2]}\n\n')
+
+def getPrompt(p, h):
+    return f"""You are a mathematician working on logic statements. Your task is to determine whether the premise entails the hypothesis, just like the examples below.
+
+Premise: We use a number of interesting categories related to probability theory.
+Hypothesis: There exists a number of interesting categories related to probability theory.
+Label: Entailment
+
+Premise: We use a number of interesting categories related to probability theory.
+Hypothesis: There are no interesting categories related to probability theory.
+Label: Contradition
+
+Premise: We use a number of interesting categories related to probability theory.
+Hypothesis: There are no interesting categories related to topology.
+Label: Neutral
+
+Premise: {p}
+Hypothesis: {h}
+Label:""".replace('\n','\\n')
+
+import pandas as pd
+prompts = [getPrompt(i[0],i[1]) for i in new]
+df = pd.DataFrame({'system_prompt':['You are a helpful assistant. 你是一个乐于助人的助手。']*len(prompts)})
+df['temperature']=1
+df['top_p']=.9
+df['seed']=10
+df['max_tokens']=800
+df['prompt']=prompts
+print(df[:1].prompt.tolist()[0])
+df.to_excel('mathnli-test.xlsx')
